e02ce5692932ec19aa877429e5a68ffc28f72540
tbathat <tabathapradier@gmail.com>
2023-05-24 12:07:34 -0300

    Implement chat log

--- web/src/App.js
+++ web/src/App.js
@@ -5,19 +5,46 @@ import "./styles/reset.css";
 
 import makeRequest from "./api/api";
 import { SideMenu } from "./components/SideMenu/SideMenu";
+import ChatMessage from "./components/ChatMessage";
 
 function App() {
-
   const [input, setInput] = useState("");
-  const [chatlog, setChatlog] = useState([{
-    user: "gpt",
-    message: "Como posso te ajudar hoje?"
-  }]);
+  const [chatlog, setChatlog] = useState([
+    {
+      user: "gpt",
+      message: "Como posso te ajudar hoje?",
+    },
+  ]);
+
+  async function handleSubmit(e) {
+    
+  }
 
   return (
     <div className="App">
       <SideMenu></SideMenu>
-      <h1>App Works!</h1>
+
+      <section className="chatbox">
+        <div className="chat-log">
+          {chatlog.map((message, index) => (
+            <ChatMessage 
+            key={index}
+              message={message}
+            />
+          ))}
+        </div>
+        
+        <div className="chat-input-holder">
+          <form onSubmit={handleSubmit}>
+            <input
+              rows='1'
+              className="chat-input-textarea"
+              value={input}
+              onChange={e => setInput(e.target.value)}
+            />
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
