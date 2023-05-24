b47ce957d5ee1b46705c047300aa75de93a839fe
tbathat <tabathapradier@gmail.com>
2023-05-24 12:19:07 -0300

    Create form and prevent events

--- web/src/App.js
+++ web/src/App.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 import "./styles/App.css";
 import "./styles/reset.css";
 
-import makeRequest from "./api/api";
+import { makeRequest } from "./api/api";
 import { SideMenu } from "./components/SideMenu/SideMenu";
-import ChatMessage from "./components/ChatMessage";
+import { ChatMessage } from "./components/ChatMessage/ChatMessage";
 
 function App() {
   const [input, setInput] = useState("");
@@ -17,7 +17,25 @@ function App() {
   ]);
 
   async function handleSubmit(e) {
-    
+    e.preventDefault();
+
+    let response = await makeRequest({ prompt: input });
+
+    response = response.data.split("\n").map((line) => <p>{line}</p>);
+
+    setChatlog([
+      ...chatlog,
+      {
+        user: "me",
+        message: `${input}`,
+      },
+      {
+        user: "gpt",
+        message: response,
+      },
+    ]);
+
+    setInput("");
   }
 
   return (
@@ -27,20 +45,17 @@ function App() {
       <section className="chatbox">
         <div className="chat-log">
           {chatlog.map((message, index) => (
-            <ChatMessage 
-            key={index}
-              message={message}
-            />
+            <ChatMessage key={index} message={message} />
           ))}
         </div>
-        
+
         <div className="chat-input-holder">
           <form onSubmit={handleSubmit}>
             <input
-              rows='1'
+              rows="1"
               className="chat-input-textarea"
               value={input}
-              onChange={e => setInput(e.target.value)}
+              onChange={(e) => setInput(e.target.value)}
             />
           </form>
         </div>
